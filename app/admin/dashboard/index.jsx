f5645739904590c33eb18359bc87074dcdccb60f
Nirmal21D <nirmaldarekar90@gmail.com>
2025-03-01 15:35:16 +0530

    video

--- app/admin/dashboard/index.jsx
+++ app/admin/dashboard/index.jsx
@@ -165,7 +165,7 @@ export default function AdminDashboard() {
                     </Animated.View>
                 </ScrollView>
             </SafeAreaView>
-        </View>
+        </ScrollView>
     );
 }
 
